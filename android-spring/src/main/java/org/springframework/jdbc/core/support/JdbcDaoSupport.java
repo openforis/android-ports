4bde85e9b8b580d4a935bd514b61af1b53ff2417
Ricci Stefano <stefano.ricci@fao.org>
2013-07-02 17:26:51 +0200

    Removed useless stuff

--- android-spring/src/main/java/org/springframework/jdbc/core/support/JdbcDaoSupport.java
+++ android-spring/src/main/java/org/springframework/jdbc/core/support/JdbcDaoSupport.java
@@ -5,62 +5,26 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-//move to new project
-public class JdbcDaoSupport {
+/**
+ * 
+ * @author S. Ricci
+ * @author K. Waga
+ *
+ */
+public abstract class JdbcDaoSupport {
 
-	private static Connection connection;
-	private static DataSource dataSource;
+	private DataSource dataSource;
 	
-	public static void init(DataSource dataSource) {
-		JdbcDaoSupport.dataSource = dataSource;
+	public Connection getConnection() throws SQLException {
+		return dataSource.getConnection();
 	}
 	
-	public Connection getConnection() {
-		try {
-			if ( connection == null || connection.isClosed() ) {
-				connection = dataSource.getConnection();
-			}
-		} catch (Exception e) {
-			connection = null;
-		} 
-		return connection;
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 	
-	public static void commit() {
-		try {
-			connection.commit();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
+	public DataSource getDataSource() {
+		return dataSource;
 	}
 	
-	public static void close() {
-		try {
-			connection.close();
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	public static boolean isOpen(){
-		try {
-			return !connection.isClosed();
-		} catch (SQLException e) {
-			return false;
-		} catch (NullPointerException e){
-			return false;
-		}
-	}
-	
-	public static boolean isClosed(){
-		if ( connection == null ) {
-			return false;
-		} else {
-			try {
-				return connection.isClosed();
-			} catch (SQLException e) {
-				return true;
-			}
-		}
-	}
 }
