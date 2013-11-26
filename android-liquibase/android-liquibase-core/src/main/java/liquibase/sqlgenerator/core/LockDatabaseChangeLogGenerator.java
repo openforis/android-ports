5968e341b2e3cafab607143b0e2eb7607a5326fd
Stefano Ricci <stefano.ricci@fao.org>
2013-11-26 21:38:32 +0100

    Avoid access network resources while instanciating Liquibase objects

--- android-liquibase/android-liquibase-core/src/main/java/liquibase/sqlgenerator/core/LockDatabaseChangeLogGenerator.java
+++ android-liquibase/android-liquibase-core/src/main/java/liquibase/sqlgenerator/core/LockDatabaseChangeLogGenerator.java
@@ -1,19 +1,16 @@
 package liquibase.sqlgenerator.core;
 
+import java.sql.Timestamp;
+
 import liquibase.database.Database;
+import liquibase.database.typeconversion.TypeConverter;
 import liquibase.database.typeconversion.TypeConverterFactory;
-import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.exception.ValidationErrors;
 import liquibase.sql.Sql;
-import liquibase.sqlgenerator.SqlGenerator;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.SqlGeneratorFactory;
 import liquibase.statement.core.LockDatabaseChangeLogStatement;
 import liquibase.statement.core.UpdateStatement;
-import liquibase.util.NetUtil;
-
-import java.net.InetAddress;
-import java.sql.Timestamp;
 
 public class LockDatabaseChangeLogGenerator extends AbstractSqlGenerator<LockDatabaseChangeLogStatement> {
 
@@ -21,31 +18,21 @@ public class LockDatabaseChangeLogGenerator extends AbstractSqlGenerator<LockDat
         return new ValidationErrors();
     }
 
-    private static String hostname;
-    private static String hostaddress;
-
-    static {
-        InetAddress localHost;
-        try {
-            localHost = NetUtil.getLocalHost();
-            hostname = localHost.getHostName();
-            hostaddress = localHost.getHostAddress();
-        } catch (Exception e) {
-            throw new UnexpectedLiquibaseException(e);
-        }
-    }
-
     public Sql[] generateSql(LockDatabaseChangeLogStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
-    	String liquibaseSchema = null;
-    		liquibaseSchema = database.getLiquibaseSchemaName();
+    	String liquibaseSchema = database.getLiquibaseSchemaName();
 
         UpdateStatement updateStatement = new UpdateStatement(liquibaseSchema, database.getDatabaseChangeLogLockTableName());
         updateStatement.addNewColumnValue("LOCKED", true);
         updateStatement.addNewColumnValue("LOCKGRANTED", new Timestamp(new java.util.Date().getTime()));
-        updateStatement.addNewColumnValue("LOCKEDBY", hostname + " (" + hostaddress + ")");
-        updateStatement.setWhereClause(database.escapeColumnName(liquibaseSchema, database.getDatabaseChangeLogTableName(), "ID") + " = 1 AND " + database.escapeColumnName(liquibaseSchema, database.getDatabaseChangeLogTableName(), "LOCKED") + " = "+ TypeConverterFactory.getInstance().findTypeConverter(database).getBooleanType().getFalseBooleanValue());
+        updateStatement.addNewColumnValue("LOCKEDBY", "localhost");
+        
+        TypeConverter typeConverter = TypeConverterFactory.getInstance().findTypeConverter(database);
+        String idColumnName = database.escapeColumnName(liquibaseSchema, database.getDatabaseChangeLogTableName(), "ID");
+		String lockedColumnName = database.escapeColumnName(liquibaseSchema, database.getDatabaseChangeLogTableName(), "LOCKED");
+		String falseBooleanValue = typeConverter.getBooleanType().getFalseBooleanValue();
+		
+		updateStatement.setWhereClause(idColumnName + " = 1 AND " + lockedColumnName + " = "+ falseBooleanValue);
 
         return SqlGeneratorFactory.getInstance().generateSql(updateStatement, database);
-
     }
 }
